3106f39cb69de0e845da09493bf0ddc1899aac6d
Qulive-edu <assasinplay306@gmail.com>
2024-11-25 16:50:45 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js in Docker!');
+  res.send('Nodejs is up');
 });
 
 const port = 3000;
